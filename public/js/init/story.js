092e38fa4b9bcf6d82366f4bcaaadd3554bf996d
krushndayshmookh <krushndayshmookh@gmail.com>
2019-05-14 02:06:27 +0530

    cleanup

--- public/js/init/story.js
+++ public/js/init/story.js
@@ -1,12 +1,12 @@
 var carouselinstance
 document.addEventListener('DOMContentLoaded', function() {
-	var carouselelem = document.querySelector('#herocarousel')
-	carouselinstance = M.Carousel.init(carouselelem, {
-		fullWidth: true,
-		indicators: true
-	})
+	// var carouselelem = document.querySelector('#herocarousel')
+	// carouselinstance = M.Carousel.init(carouselelem, {
+	// 	fullWidth: true,
+	// 	indicators: true
+	// })
 
-	setInterval(function() {
-		carouselinstance.next()
-	}, 3000)
+	// setInterval(function() {
+	// 	carouselinstance.next()
+	// }, 3000)
 })
