2a40c570fbe9fbd810197ecf5f6ac451ae66dc63
krushndayshmookh <krushndayshmookh@gmail.com>
2019-05-16 15:32:19 +0530

    add particles

--- public/js/init/story.js
+++ public/js/init/story.js
@@ -1,12 +1,3 @@
-var carouselinstance
 document.addEventListener('DOMContentLoaded', function() {
-	// var carouselelem = document.querySelector('#herocarousel')
-	// carouselinstance = M.Carousel.init(carouselelem, {
-	// 	fullWidth: true,
-	// 	indicators: true
-	// })
-
-	// setInterval(function() {
-	// 	carouselinstance.next()
-	// }, 3000)
+	particlesJS.load('particles', '/js/init/particles.json')
 })
