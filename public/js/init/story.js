ea22b662be2b6e2ca5c7547479f2ad4266923b8d
krushndayshmookh <krushndayshmookh@gmail.com>
2019-06-16 20:49:40 +0530

    add masonry and images loaded

--- public/js/init/story.js
+++ public/js/init/story.js
@@ -4,4 +4,20 @@ document.addEventListener('DOMContentLoaded', function() {
 	M.Slider.init(document.querySelectorAll('.slider'), {
 		// height: 600
 	})
+
+	// Masonry Grid
+	var $masonry = $('.artmenu')
+	$masonry.masonry({
+		// set itemSelector so .grid-sizer is not used in layout
+		itemSelector: '.artmenuitem',
+		// use element for option
+		columnWidth: '.artmenuitem',
+		// no transitions
+		transitionDuration: 0
+	})
+	// layout Masonry after each image loads
+	$masonry.imagesLoaded(function() {
+		$masonry.masonry('layout')
+	})
+	
 })
